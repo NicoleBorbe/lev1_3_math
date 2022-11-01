f89888c9a92b80bd5b842c1bb107eadacdce1314
NicoleBorbe <nicole.borbe@gmail.com>
2022-11-01 21:55:04 +0100

    version1

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let randomNum = Math.random();
+console.log(randomNum);
+
+let randomNum1_10 = Math.ceil(Math.random() * 10);
+console.log(randomNum1_10);
+
+let randomNum1_100 = Math.ceil(Math.random() * 100);
+console.log(randomNum1_100);
